76cf1822235f3b1f3e85ccdcba9cdecf5e084322
sawel <sawel@yandex.ru>
2018-12-08 14:30:20 +0800

    ver 1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+// require packages
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    maps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleancss = require('gulp-clean-css'),
+    del = require('del'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    babel = require('gulp-babel'),
+    imagemin = require('gulp-imagemin'),
+    gIf = require('gulp-if');
+
+
+// define isProd
+const isProd = false;
+
+// define paths
+const paths = {
+    root: {
+        src: 'src',
+        dest: 'app'
+    },
+    html: {
+        src: 'src/*.html',
+        dest: 'app'
+    },
+    styles: {
+        src: 'src/scss/*.scss',
+        dest: 'app/assets/styles/'
+    },
+    scripts: {
+        src: 'src/js/*.js',
+        dest: 'app/assets/scripts/'
+    },
+    images: {
+        src: 'src/images/*.{jpg,jpeg,png}',
+        dest: 'app/assets/images/'
+    }
+};
+
+// define tasks
+function clean() {
+    return del(['app/assets']);
+}
+
+function html() {
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.dest))
+        .pipe(connect.reload());
+}
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(gIf(!isProd, maps.init()))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false,
+            grid: "autoplace"
+        }))
+        .pipe(cleancss())
+        .pipe(gIf(!isProd, maps.write()))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(connect.reload());
+}
+
+function scripts() {
+    return gulp.src(paths.scripts.src)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gIf(isProd, uglify()))
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(connect.reload());
+}
+
+function images() {
+    return gulp.src(paths.images.src, {since: gulp.lastRun(images)})
+        .pipe(imagemin({optimizationLevel: 5}))
+        .pipe(gulp.dest(paths.images.dest))
+        .pipe(connect.reload());
+}
+
+function serve() {
+    connect.server({
+        root: paths.root.dest,
+        livereload: true
+    });
+}
+
+function watch() {
+    gulp.watch(paths.html.src, html);
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.images.src, images);
+}
+
+gulp.task('default', gulp.series(clean, gulp.parallel(html, styles, scripts, images, serve, watch)));
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(() => {
+    let body = $('body');
+    $('.gruv').on('click', () => {body.css('background-color', 'red');console.log("привет")})
+});
